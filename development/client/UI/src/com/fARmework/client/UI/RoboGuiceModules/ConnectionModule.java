d10ff37a0d87193a352854bb13215fb788158d3b
manisero <manisero@manisero-PC.home.aster.pl>
2012-07-14 23:21:32 +0200

    Moved Client.* projects to dedicated directory

---
+++ development/client/UI/src/com/fARmework/client/UI/RoboGuiceModules/ConnectionModule.java
@@ -0,0 +1,14 @@
+package com.fARmework.client.UI.RoboGuiceModules;
+
+import com.fARmework.client.Connection.IConnectionManager;
+import com.fARmework.client.Connection.Impl.NettyConnectionManager;
+import com.google.inject.AbstractModule;
+
+public class ConnectionModule extends AbstractModule
+{
+	@Override
+	protected void configure()
+	{
+		bind(IConnectionManager.class).to(NettyConnectionManager.class);
+	}
+}
