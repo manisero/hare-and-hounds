a37fdc5e9a1b94c28afd6a16062dd3aa4585d9ea
manisero <manisero@manisero-PC.home.aster.pl>
2012-07-24 22:40:15 +0200

    Updated Server project so that its structure reflects Client project

--- development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/LogicModule.java
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/LogicModule.java
@@ -2,10 +2,6 @@ package com.fARmework.RockPaperScissors.Server.GuiceModules;
 
 import com.fARmework.RockPaperScissors.Server.Logic.IConnectionHandler;
 import com.fARmework.RockPaperScissors.Server.Logic.Impl.ConnectionHandler;
-import com.fARmework.RockPaperScissors.Server.Logic.Impl.MessageProcessor;
-import com.fARmework.core.server.IMessageProcessor;
-import com.fARmework.core.server.IConnectionManager;
-import com.fARmework.core.server.impl.ConnectionManager;
 import com.google.inject.AbstractModule;
 
 public class LogicModule extends AbstractModule
@@ -13,8 +9,6 @@ public class LogicModule extends AbstractModule
 	@Override
 	protected void configure()
 	{
-		bind(IConnectionManager.class).to(ConnectionManager.class);
-		bind(IMessageProcessor.class).to(MessageProcessor.class);
-		bind(IConnectionHandler.class).to(ConnectionHandler.class);
+		bind(IConnectionHandler.class).to(ConnectionHandler.class).asEagerSingleton();
 	}
 }
---
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/Logic/DataHandlers/IDataHandler.java
@@ -0,0 +1,6 @@
+package com.fARmework.RockPaperScissors.Server.Logic.DataHandlers;
+
+public interface IDataHandler<T>
+{
+	void handle(T data);
+}
