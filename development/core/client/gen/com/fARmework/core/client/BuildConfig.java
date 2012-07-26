6e1d6633678bb389e5bde0dbfa7404f1f5133563
manisero <manisero@manisero-PC.home.aster.pl>
2012-07-26 22:06:19 +0200

    Merged Client.Connection and Client.Infrastructure projects

--- development/core/client/gen/com/fARmework/core/client/BuildConfig.java
+++ development/core/client/gen/com/fARmework/core/client/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.fARmework.client.Infrastructure;
+package com.fARmework.core.client;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
