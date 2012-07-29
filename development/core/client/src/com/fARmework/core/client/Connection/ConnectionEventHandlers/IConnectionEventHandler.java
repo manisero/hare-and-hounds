b33a2ca56dd05f0245cc46be5a9799bfaf0a9fe9
manisero <manisero@manisero-PC.home.aster.pl>
2012-07-30 01:16:55 +0200

    Got rid of ConnectionEventHandlers

--- development/core/client/src/com/fARmework/core/client/Connection/ConnectionEventHandlers/IConnectionEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.fARmework.core.client.Connection.ConnectionEventHandlers;
-
-import com.fARmework.core.client.Connection.IConnectionHandler;
-
-public interface IConnectionEventHandler
-{
-	void handleWith(IConnectionHandler connectionHandler);
-}
