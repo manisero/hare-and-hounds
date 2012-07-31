9ac42788008070d13186f15cee5eb2c49574832d
Kuba <Kuba@kubPC.Home>
2012-07-31 16:16:13 +0200

    Implemented motion drawing on screen

--- creativity/Android/GestureWriter/src/com/fARmework/creat/GestureWriter/MainActivity.java
+++ creativity/Android/GestureWriter/src/com/fARmework/creat/GestureWriter/MainActivity.java
@@ -1,19 +1,21 @@
 package com.fARmework.creat.GestureWriter;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.*;
+import android.os.*;
+import android.view.*;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity 
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
