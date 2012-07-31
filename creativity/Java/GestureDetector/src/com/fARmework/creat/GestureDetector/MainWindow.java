26d002bd7bdb977c52eef0696f6fe3523611a90c
Kuba <Kuba@kubPC.Home>
2012-07-31 13:58:56 +0200

    Implemented reading gestures from files

--- creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetector/MainWindow.java
+++ creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetector/MainWindow.java
@@ -1,5 +1,6 @@
 package com.fARmework.creat.GestureDetector;
 
+import java.io.*;
 import javax.swing.*;
 
 public class MainWindow extends JFrame
@@ -8,7 +9,13 @@ public class MainWindow extends JFrame
 	
 	public MainWindow()
 	{
+		GestureFileReader fileReader = new GestureFileReader(new File("testFile.txt"));
 		
+		fileReader.readFromFile();
+		
+		fileReader.print();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String args[])
