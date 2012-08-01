fef6aef278b63a4e8f703fa6e5888e610a5f9572
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-02 01:21:25 +0200

    Fixed NettyConnectionManager resolution (it crashed on attempt to connect after exiting and then starting the application)

--- development/RockPaperScissors/Client/src/com/fARmework/RockPaperScissors/Client/RoboGuiceModules/CoreModule.java
+++ development/RockPaperScissors/Client/src/com/fARmework/RockPaperScissors/Client/RoboGuiceModules/CoreModule.java
@@ -2,7 +2,6 @@ package com.fARmework.RockPaperScissors.Client.RoboGuiceModules;
 
 import com.fARmework.RockPaperScissors.Client.Infrastructure.SettingsProvider;
 import com.fARmework.core.client.Connection.IConnectionManager;
-import com.fARmework.core.client.Connection.Impl.NettyConnectionManager;
 import com.fARmework.core.client.Infrastructure.ISettingsProvider;
 import com.fARmework.core.data.IDataRegistry;
 import com.fARmework.core.data.IDataService;
@@ -23,6 +22,6 @@ public class CoreModule extends AbstractModule
 		bind(ISettingsProvider.class).to(SettingsProvider.class);
 		
 		// core.Client.Connection
-		bind(IConnectionManager.class).to(NettyConnectionManager.class).asEagerSingleton();
+		bind(IConnectionManager.class).toProvider(ConnectionManagerProvider.class);
 	}
 }
