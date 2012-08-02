a5e5998c3a2c9b9875d29ca42dfd09f6c821c64a
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-02 23:44:56 +0200

    Removed Main activity, view model and view

--- development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/Logic/DataHandlers/DataHandler.java
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/Logic/DataHandlers/DataHandler.java
@@ -7,7 +7,7 @@ public abstract class DataHandler<T> implements IDataHandler<T>
 	@Override
 	public void handle(int clientID, T data)
 	{
-		String dataClassName = data.getClass().getCanonicalName();
+		String dataClassName = data.getClass().getName();
 		String dataTypeName = dataClassName.subSequence(dataClassName.lastIndexOf('.') + 1, dataClassName.length()).toString();
 		
 		System.out.println(dataTypeName + " received (clientID = " + clientID + ")");
