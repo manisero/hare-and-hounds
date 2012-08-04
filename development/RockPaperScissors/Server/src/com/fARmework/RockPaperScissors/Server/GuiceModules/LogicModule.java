da555deb98ae34055af87c5bb5d746505d591dcc
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-04 20:12:48 +0200

    - renamed RockPaperScissors GestureData to GestureInfo - added abstract handleData method to DataHandler - created IGestureProcessor interface and moved GestureProcessor to Logic package

--- development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/LogicModule.java
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/LogicModule.java
@@ -1,7 +1,9 @@
 package com.fARmework.RockPaperScissors.Server.GuiceModules;
 
 import com.fARmework.RockPaperScissors.Server.Logic.IGameManager;
+import com.fARmework.RockPaperScissors.Server.Logic.IGestureProcessor;
 import com.fARmework.RockPaperScissors.Server.Logic.Impl.GameManager;
+import com.fARmework.RockPaperScissors.Server.Logic.Impl.GestureProcessor;
 import com.google.inject.AbstractModule;
 
 public class LogicModule extends AbstractModule
@@ -10,5 +12,6 @@ public class LogicModule extends AbstractModule
 	protected void configure()
 	{
 		bind(IGameManager.class).to(GameManager.class);
+		bind(IGestureProcessor.class).to(GestureProcessor.class);
 	}
 }
