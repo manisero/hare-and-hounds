23937ed9637c1063c3e2f6af8cf8f481a861af6c
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-04 21:45:36 +0200

    Implemented DataHandlersCollection

--- development/core/Server/src/com/fARmework/core/server/Connection/Impl/DataHandlersCollection.java
+++ development/core/Server/src/com/fARmework/core/server/Connection/Impl/DataHandlersCollection.java
@@ -1,48 +1,86 @@
 package com.fARmework.core.server.Connection.Impl;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.fARmework.core.server.Connection.IDataHandler;
 
+@SuppressWarnings("rawtypes")
 public class DataHandlersCollection
 {
+	private Map<Class<?>, IDataHandler> _commonDataHandlers = new LinkedHashMap<Class<?>, IDataHandler>();
+	private Map<Integer, Map<Class<?>, IDataHandler>> _individualDataHandlers = new LinkedHashMap<Integer, Map<Class<?>, IDataHandler>>();
+	
 	public <T> void register(Class<T> dataClass, IDataHandler<T> handler)
 	{
-		// TODO Auto-generated method stub
+		_commonDataHandlers.put(dataClass, handler);
 	}
 	
 	public <T> void register(Class<T> dataClass, IDataHandler<T> handler, int clientID)
 	{
-		// TODO Auto-generated method stub
+		if (!_individualDataHandlers.containsKey(clientID))
+		{
+			_individualDataHandlers.put(clientID, new LinkedHashMap<Class<?>, IDataHandler>());
+		}
+		
+		_individualDataHandlers.get(clientID).put(dataClass, handler);
 	}
 	
 	public <T> void register(Class<T> dataClass, IDataHandler<T> handler, List<Integer> clientIDs)
 	{
-		// TODO Auto-generated method stub
+		for (Integer clientID : clientIDs)
+		{
+			register(dataClass, handler, clientID);
+		}
 	}
 	
 	public void clear()
 	{
-		// TODO Auto-generated method stub
+		_commonDataHandlers.clear();
+		_individualDataHandlers.clear();
 	}
 	
 	public void clear(int clientID)
 	{
-		// TODO Auto-generated method stub
+		_individualDataHandlers.remove(clientID);
 	}
 	
 	public void clear(List<Integer> clientIDs)
 	{
-		// TODO Auto-generated method stub
+		for (Integer clientID : clientIDs)
+		{
+			clear(clientID);
+		}
+	}
+	
+	public <T> void clear(Class<T> dataClass)
+	{
+		_commonDataHandlers.remove(dataClass);
+		
+		for (Integer clientID : _individualDataHandlers.keySet())
+		{
+			clear(dataClass, clientID);
+		}
 	}
 	
 	public <T> void clear(Class<T> dataClass, int clientID)
 	{
-		// TODO Auto-generated method stub
+		if (_individualDataHandlers.containsKey(clientID))
+		{
+			Map<Class<?>, IDataHandler> individualMap = _individualDataHandlers.get(clientID);
+			individualMap.remove(dataClass);
+			
+			if (individualMap.size() == 0)
+				_individualDataHandlers.remove(clientID);
+		}
 	}
 	
 	public <T> void clear(Class<T> dataClass, List<Integer> clientIDs)
 	{
-		// TODO Auto-generated method stub
+		for (Integer clientID : clientIDs)
+		{
+			clear(dataClass, clientID);
+		}
 	}
 }
