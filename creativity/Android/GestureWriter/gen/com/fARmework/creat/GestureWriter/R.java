639938ddfd0d3fff1d406e6cbe74f0ce9f904b8a
Kuba <Kuba@kubPC.Home>
2012-08-05 16:52:24 +0200

    Implemented gesture capturing to a file

--- creativity/Android/GestureWriter/gen/com/fARmework/creat/GestureWriter/R.java
+++ creativity/Android/GestureWriter/gen/com/fARmework/creat/GestureWriter/R.java
@@ -15,8 +15,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int MainView1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int clear_button=0x7f070001;
+        public static final int main_view=0x7f070000;
+        public static final int menu_settings=0x7f070003;
+        public static final int save_button=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,8 +28,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int clear_string=0x7f040005;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int save_string=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
