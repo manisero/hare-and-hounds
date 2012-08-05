8bb380d6efa6b07ee85dfdfbcaddf990e60d4949
Kuba <Kuba@kubPC.Home>
2012-08-05 19:59:09 +0200

    Implemented scratch of rectangle-based detection technology

--- creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetector/GestureDrawer.java
+++ creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetector/GestureDrawer.java
@@ -7,13 +7,15 @@ import java.util.*;
 
 public class GestureDrawer 
 {
+	private static int RECTANGLE_SIZE = 15;
+	
 	public BufferedImage drawGesture(GestureData data)
 	{
 		Rectangle boundingBox = getBoundingBox(data);
 		
 		BufferedImage image = new BufferedImage(boundingBox.width,
 												boundingBox.height,
-												BufferedImage.TYPE_BYTE_GRAY);
+												BufferedImage.TYPE_INT_RGB);
 		
 		for(int x = 0; x < boundingBox.width; ++x)
 		{
@@ -33,10 +35,53 @@ public class GestureDrawer
 			image.setRGB(x, y, Color.black.getRGB());
 		}
 		
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		
+		for(int x = 0; x < boundingBox.width; x += RECTANGLE_SIZE + 1)
+		{
+			for(int y = 0; y < boundingBox.height; y += RECTANGLE_SIZE + 1)
+			{
+				Rectangle rectangle = new Rectangle(x, y, RECTANGLE_SIZE, RECTANGLE_SIZE);
+				
+				if(isRectangleFilled(data, rectangle))
+				{
+					graphics.setColor(Color.RED);
+				}
+				else
+				{
+					graphics.setColor(Color.BLACK);
+				}
+				
+				graphics.drawRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+			}
+		}
+		
+		graphics.dispose();
+		
 		return image;
 	}
 	
-	private Rectangle getBoundingBox(GestureData data)
+	private boolean isRectangleFilled(GestureData data, Rectangle rectangle)
+	{
+		LinkedList<GestureData.Point> points = data.Points;
+		
+		Rectangle boundingBox = getBoundingBox(data);
+		
+		for(GestureData.Point point : points)
+		{
+			int x = (int) point.X - boundingBox.x;
+			int y = (int) point.Y - boundingBox.y;
+			
+			if(rectangle.contains(x, y))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public Rectangle getBoundingBox(GestureData data)
 	{
 		LinkedList<GestureData.Point> points = data.Points;
 		
