4eaf3f2671e01caf5d50a98d3ad6ecc1799b773e
Kuba <Kuba@kubPC.Home>
2012-08-06 17:00:14 +0200

    Implemented gathering gestures from multiple files

--- creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetectory/EntryPoint/EntryPoint.java
+++ creativity/Java/GestureDetector/src/com/fARmework/creat/GestureDetectory/EntryPoint/EntryPoint.java
@@ -7,6 +7,7 @@ import com.fARmework.modules.ScreenGestures.Data.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
+
 import javax.swing.*;
 
 public class EntryPoint extends JFrame
@@ -18,8 +19,24 @@ public class EntryPoint extends JFrame
 	
 	private IGesturesDetector _gesturesDetector;
 	
+	private static final String _directory = "samples";
+	private static final String[] _files = {
+		"circle_counterclockwise.dat",
+		"circle_clockwise.dat",
+		"cross1.dat",
+		"cross2.dat",
+		"cross3.dat",
+		"square_clockwise.dat",
+		"square_counterclockwise.dat",
+		"triangle_clockwise.dat",
+		"triangle_counterclockwise.dat"
+	};
+	
+	private Set<String> _filesWithGestures;
+	
 	public EntryPoint()
 	{
+		_filesWithGestures = new LinkedHashSet<String>();
 		_fileReader = new GestureFileReader(new File("samples/square_clockwise.dat"));
 		_gestureDrawer = new GestureDrawer();
 		
@@ -32,29 +49,43 @@ public class EntryPoint extends JFrame
 	
 	public void detect()
 	{
-		LinkedList<GestureData> gestures = _fileReader.getGestures();
-		
-		for(GestureData data : gestures)
-		{	
-			String gestureType = _gesturesDetector.recognizeGesture(data);
+		for(String file : _files)
+		{
+			_fileReader.clear();
+			_fileReader.setFile(new File(_directory + "/" + file));
 			
-			if(_gesturesDetector.unrecognizedGestureString().equals(gestureType))
-			{
-				System.out.println("Unrecognized gesture");
-				continue;
-			}
+			LinkedList<GestureData> gestures = _fileReader.getGestures();
+			
+			for(GestureData data : gestures)
+			{	
+				String gestureType = _gesturesDetector.recognizeGesture(data);
 				
-			BufferedImage image = _gestureDrawer.drawGesture(data);
-			
-			JFrame frame = new JFrame();
-			JLabel label = new JLabel();
-			
-			label.setIcon(new ImageIcon(image));
-			frame.add(label);
-			frame.setTitle("Recognized: " + gestureType);
-			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			frame.pack();
-			frame.setVisible(true);
+				if(_gesturesDetector.unrecognizedGestureString().equals(gestureType))
+				{
+					continue;
+				}
+					
+				BufferedImage image = _gestureDrawer.drawGesture(data);
+				
+				JFrame frame = new JFrame();
+				JLabel label = new JLabel();
+				
+				label.setIcon(new ImageIcon(image));
+				frame.add(label);
+				frame.setTitle("Recognized: " + gestureType + " in file: " + file);
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				frame.pack();
+				frame.setVisible(true);
+				
+				_filesWithGestures.add(file);
+			}
+		}
+		
+		System.out.println("Gestures found in files: ");
+		
+		for(String filename : _filesWithGestures)
+		{
+			System.out.println("* " + filename);
 		}
 	}
 
