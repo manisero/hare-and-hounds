cd9a03a3f51b73787e42e687d2f92734761df00e
Kuba <Kuba@kubPC.Home>
2012-08-07 18:23:24 +0200

    Added test project for gesture detector

---
+++ creativity/Java/GestureDetectorTests/test/com/fARmework/creat/GestureDetector/Test/DefaultGestureProcessorTest.java
@@ -0,0 +1,15 @@
+package com.fARmework.creat.GestureDetector.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultGestureProcessorTest {
+
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
