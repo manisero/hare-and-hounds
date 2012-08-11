ad0b91079039ff26c5b9f8dcdd9f805a997efeb9
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-12 00:40:15 +0200

    Replaced IGesture interface with Gesture abstract class

--- development/modules/ScreenGestures.Java/src/com/fARmework/modules/ScreenGestures/Java/Gestures/PlainGesture.java
+++ development/modules/ScreenGestures.Java/src/com/fARmework/modules/ScreenGestures/Java/Gestures/PlainGesture.java
@@ -1,27 +1,9 @@
 package com.fARmework.modules.ScreenGestures.Java.Gestures;
 
-import com.fARmework.modules.ScreenGestures.Java.*;
-
-public class PlainGesture implements IGesture<Boolean>
+public class PlainGesture extends Gesture<Boolean>
 {
-	private Boolean[][] _pattern;
-	private String _name;
-	
-	public PlainGesture(Boolean[][] pattern, String name)
+	public PlainGesture(String name, Boolean[][] pattern)
 	{
-		_pattern = pattern;
-		_name = name;
-	}
-	
-	@Override
-	public Boolean[][] getPattern() 
-	{
-		return _pattern;
-	}
-
-	@Override
-	public String getName() 
-	{
-		return _name;
+		super(name, pattern);
 	}
 }
