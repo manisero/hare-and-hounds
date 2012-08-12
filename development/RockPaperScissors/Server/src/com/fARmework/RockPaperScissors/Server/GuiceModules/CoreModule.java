69675ad1b8eef1056b2caf28ef5068073cde449b
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-13 00:05:25 +0200

    Started removing redundant libs from SpaceGestures project

---
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/CoreModule.java
@@ -0,0 +1,28 @@
+package com.fARmework.RockPaperScissors.Server.GuiceModules;
+
+import com.fARmework.RockPaperScissors.Server.SettingsProvider;
+import com.fARmework.core.data.IDataRegistry;
+import com.fARmework.core.data.IDataService;
+import com.fARmework.core.data._impl.DataRegistry;
+import com.fARmework.core.data._impl.DataService;
+import com.fARmework.core.server.Connection.IConnectionManager;
+import com.fARmework.core.server.Connection._impl.ConnectionManager;
+import com.fARmework.core.server.Infrastructure.ISettingsProvider;
+import com.google.inject.AbstractModule;
+
+public class CoreModule extends AbstractModule
+{
+	@Override
+	protected void configure()
+	{
+		// core.Data
+		bind(IDataRegistry.class).to(DataRegistry.class).asEagerSingleton();
+		bind(IDataService.class).to(DataService.class);
+		
+		// core.Server.Infrastructure
+		bind(ISettingsProvider.class).to(SettingsProvider.class);
+		
+		// core.Server.Connection
+		bind(IConnectionManager.class).to(ConnectionManager.class).asEagerSingleton();
+	}
+}
---
+++ development/modules/SpaceGestures/Android/src/com/fARmework/modules/SpaceGestures/Android/SpaceGesturePicker.java
@@ -0,0 +1,98 @@
+package com.fARmework.modules.SpaceGestures.Android;
+
+import com.fARmework.modules.SpaceGestures.Data.SpaceGestureData;
+
+import gueei.binding.Command;
+import gueei.binding.IBindableView;
+import gueei.binding.ViewAttribute;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SpaceGesturePicker extends View implements IBindableView<SpaceGesturePicker>
+{
+	private SpaceGestureRecorder _spaceGestureRecorder = new SpaceGestureRecorder();
+	private OnSpaceGestureListener _gestureListener;
+	
+	// onGesture attribute (Android-Binding support)
+	private ViewAttribute<SpaceGesturePicker, Command> _onGestureAttribute =
+				new ViewAttribute<SpaceGesturePicker, Command>(Command.class, SpaceGesturePicker.this, "onGesture")
+				{
+				    @Override
+				    protected void doSetAttributeValue(final Object newValue)
+				    {
+				    	setOnGestureListener(new OnSpaceGestureListener()
+						{
+							@Override
+							public void onGesture(View v, SpaceGestureData gesture)
+							{
+								((Command)newValue).Invoke(v, gesture);
+							}
+						});
+				    }
+				
+				    @Override
+				    public Command get()
+				    {
+				    	return null;
+				    }
+				};
+	
+	public SpaceGesturePicker(Context context)
+	{
+		super(context);
+		initialize();
+	}
+	
+	public SpaceGesturePicker(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		initialize();
+	}
+
+	protected void initialize()
+	{
+		setOnTouchListener(new OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				if (_gestureListener == null)
+				{
+					return false;
+				}
+				
+				if (event.getActionMasked() == MotionEvent.ACTION_DOWN)
+				{
+					_spaceGestureRecorder.startRecording(getContext());
+				}
+				else if (event.getActionMasked() == MotionEvent.ACTION_UP)
+				{
+					_gestureListener.onGesture(v, _spaceGestureRecorder.stopRecording());
+				}
+				
+				try { Thread.sleep(500); }
+				catch (InterruptedException e) { }
+				
+				return true;
+			}
+		});
+	}
+
+	@Override
+	public ViewAttribute<? extends View, ?> createViewAttribute(String attribute)
+	{
+		if (attribute.equals("onGesture"))
+		{
+			return _onGestureAttribute;
+		}
+		
+		return null;
+	}
+	
+	public void setOnGestureListener(OnSpaceGestureListener listener)
+	{
+		_gestureListener = listener;
+	}
+}
