ced6507af67ece74f62778107a4f5390491da3e3
manisero <manisero@manisero-PC.home.aster.pl>
2012-08-12 10:27:42 +0200

    Improved GestureRecognizer "genericness", added gesture logging

--- development/modules/ScreenGestures.Java/src/com/fARmework/modules/ScreenGestures/Java/_impl/GestureRecognizer.java
+++ development/modules/ScreenGestures.Java/src/com/fARmework/modules/ScreenGestures/Java/_impl/GestureRecognizer.java
@@ -26,22 +26,48 @@ public class GestureRecognizer implements IGestureRecognizer
 	}
 
 	@Override
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public String recognize(GestureData data) 
 	{
 		List<Gesture<?>> gestures = _gestureRegistry.getGestures();
 		
-		for(Gesture<?> gesture : gestures)
+		for (Gesture<?> gesture : gestures)
 		{
-			IGestureProcessor processor = _processorFactory.get(gesture.getClass());
-			IPatternMatcher matcher = _matcherFactory.get(gesture.getClass());
+			String result = recognizeGesture(data, gesture);
 			
-			if(matcher.match(processor.getGestureGrid(data, gesture.getPattern().length), gesture.getPattern()))
-			{
-				return gesture.getName();
-			}
+			if (result != null)
+				return result;
 		}
 		
 		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private <T> String recognizeGesture(GestureData data, Gesture<T> gesture)
+	{
+		IGestureProcessor<T> processor = _processorFactory.get((Class<? extends Gesture<T>>)gesture.getClass());
+		IPatternMatcher<T> matcher = _matcherFactory.get((Class<? extends Gesture<T>>)gesture.getClass());
+		
+		T[][] grid = processor.getGestureGrid(data, gesture.getPatternSize());
+		
+		logGestureGrid(gesture.getName(), grid);
+		
+		return matcher.match(grid, gesture.getPattern()) ? gesture.getName() : null;
+	}
+	
+	private <T> void logGestureGrid(String gestureName, T[][] grid)
+	{
+		System.out.println(String.format("Gesture grid for %1$s:", gestureName));
+		
+		for (int x = 0; x != grid.length; x++)
+		{
+			for (int y = 0; y != grid[x].length; y++)
+			{
+				System.out.print(grid[x][y] + "\t");
+			}
+			
+			System.out.println();
+		}
+		
+		System.out.println();
 	}
 }
