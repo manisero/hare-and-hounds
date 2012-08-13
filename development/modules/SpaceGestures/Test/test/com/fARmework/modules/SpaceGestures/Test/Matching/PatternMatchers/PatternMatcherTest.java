ef359e9347fee33ad7db77366a8dcdc5919ff748
Kuba <Kuba@kubPC.Home>
2012-08-13 15:02:15 +0200

    Implemented CyclicSpacePatternMatcher

---
+++ development/modules/SpaceGestures/Test/test/com/fARmework/modules/SpaceGestures/Test/Matching/PatternMatchers/PatternMatcherTest.java
@@ -0,0 +1,15 @@
+package com.fARmework.modules.SpaceGestures.Test.Matching.PatternMatchers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+		CyclicSpacePatternMatcherTest.class,
+		PlainSpacePatternMatcherTest.class })
+
+public class PatternMatcherTest 
+{
+
+}
