c905196f594f023be7f1289ea30dcbd06a65e3fd
Kuba <Kuba@kubPC.Home>
2012-08-14 00:49:48 +0200

    Created ScreenGestures.Test project

---
+++ development/modules/ScreenGestures/Tests/tests/com/fARmework/modules/ScreenGestures/Tests/Matching/PatternMatchers/PlainScreenPatternMatcherTests.java
@@ -0,0 +1,13 @@
+package com.fARmework.modules.ScreenGestures.Tests.Matching.PatternMatchers;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class PlainScreenPatternMatcherTests 
+{
+	@Test
+	public void test() 
+	{
+		fail("Not yet implemented");
+	}
+}
