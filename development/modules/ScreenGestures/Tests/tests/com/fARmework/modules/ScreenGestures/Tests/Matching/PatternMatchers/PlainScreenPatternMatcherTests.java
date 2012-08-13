d662d935d61ce5a1e224f81435f83ddd4c7db59b
Kuba <Kuba@kublap.Home>
2012-08-14 01:24:01 +0200

    Implemented PlainScreenPatternMatcher tests

--- development/modules/ScreenGestures/Tests/tests/com/fARmework/modules/ScreenGestures/Tests/Matching/PatternMatchers/PlainScreenPatternMatcherTests.java
+++ development/modules/ScreenGestures/Tests/tests/com/fARmework/modules/ScreenGestures/Tests/Matching/PatternMatchers/PlainScreenPatternMatcherTests.java
@@ -3,11 +3,77 @@ package com.fARmework.modules.ScreenGestures.Tests.Matching.PatternMatchers;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import com.fARmework.modules.ScreenGestures.Java.Matching.PatternMatchers.*;
+
 public class PlainScreenPatternMatcherTests 
 {
-	@Test
-	public void test() 
+	public static class TheMatchPatternMethod
 	{
-		fail("Not yet implemented");
+		@Test
+		public void MatchesExactSamePatterns() 
+		{
+			PlainScreenPatternMatcher matcher = new PlainScreenPatternMatcher();
+			
+			Boolean[][] grid = 
+			{
+					{	true, false, true	},
+					{	true, false, true	},
+					{	true, false, true	}
+			};
+			
+			Boolean[][] pattern =
+			{
+					{	true, false, true	},
+					{	true, false, true	},
+					{	true, false, true	}
+			};
+			
+			assertTrue(matcher.match(grid, pattern));
+		}
+		
+		@Test
+		public void DiffersDistinctPatterns()
+		{
+			PlainScreenPatternMatcher matcher = new PlainScreenPatternMatcher();
+			
+			Boolean[][] grid =
+			{
+					{	false,	true,	true	},
+					{	true,	false,	false	},
+					{	false,	true,	false	}
+			};
+			
+			Boolean[][] pattern =
+			{
+					{	false,	true,	false	},
+					{	true,	true,	true	},
+					{	false,	false,	false	}
+			};
+			
+			assertFalse(matcher.match(grid, pattern));
+		}
+		
+		@Test
+		public void DiffersSizeMismatchedPatterns()
+		{
+			PlainScreenPatternMatcher matcher = new PlainScreenPatternMatcher();
+			
+			Boolean[][] grid =
+			{
+					{	true,	true,	true,	true	},
+					{	true,	true,	true,	true	},
+					{	true,	true,	true,	true	},
+					{	true,	true,	true, 	true	}
+			};
+			
+			Boolean[][] pattern =
+			{
+					{	true,	true,	true	},
+					{	true,	true,	true	},
+					{	true,	true,	true	}
+			};
+			
+			assertFalse(matcher.match(grid, pattern));
+		}
 	}
 }
