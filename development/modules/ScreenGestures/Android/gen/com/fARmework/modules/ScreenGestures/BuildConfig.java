073029e0d10f9f7b0ceac933cbf1637a2ae11041
manisero <manisero@manisero-PC>
2012-09-05 00:11:09 +0200

    Merge with RockPaperScissors-handle_host_leave

---
+++ development/modules/ScreenGestures/Android/gen/com/fARmework/modules/ScreenGestures/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fARmework.modules.ScreenGestures;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
