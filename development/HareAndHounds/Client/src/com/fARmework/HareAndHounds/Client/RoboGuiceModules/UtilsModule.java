e50e91f6d111de48b67593353b33b53443c52021
manisero <manisero@manisero-PC>
2012-09-09 18:31:32 +0200

    HareAndHounds client references Utils project

---
+++ development/HareAndHounds/Client/src/com/fARmework/HareAndHounds/Client/RoboGuiceModules/UtilsModule.java
@@ -0,0 +1,14 @@
+package com.fARmework.HareAndHounds.Client.RoboGuiceModules;
+
+import com.fARmework.utils.Android.*;
+import com.fARmework.utils.Android._impl.*;
+import com.google.inject.AbstractModule;
+
+public class UtilsModule extends AbstractModule
+{
+	@Override
+	protected void configure()
+	{
+		bind(IContextManager.class).to(ContextManager.class).asEagerSingleton();
+	}
+}
