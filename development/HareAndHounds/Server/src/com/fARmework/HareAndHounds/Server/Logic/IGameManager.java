ae515288d6447d71cef61cd62ceaeb42a46774d4
manisero <manisero@manisero-PC>
2012-09-16 14:38:46 +0200

    Implemented server-side host position tracking

--- development/HareAndHounds/Server/src/com/fARmework/HareAndHounds/Server/Logic/IGameManager.java
+++ development/HareAndHounds/Server/src/com/fARmework/HareAndHounds/Server/Logic/IGameManager.java
@@ -2,15 +2,10 @@ package com.fARmework.HareAndHounds.Server.Logic;
 
 public interface IGameManager
 {
-	public interface IGameStartHandler
-	{
-		public void onGameStart(Game game);
-	}
-	
 	public interface IGameEndHandler
 	{
 		public void onGameEnd(Game game);
 	}
 	
-	public void startGame(IGameStartHandler gameStartHandler, IGameEndHandler gameEndHandler);
+	void startGame(IGameEndHandler gameEndHandler);
 }
