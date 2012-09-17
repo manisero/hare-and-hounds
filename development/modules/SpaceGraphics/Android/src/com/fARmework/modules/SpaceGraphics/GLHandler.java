8a2257eead5d8a6f87533b8b88b27580bba90b00
Kuba <Kuba@kubPC.Home>
2012-09-17 19:38:52 +0200

    Created GLHandler to separate part of the drawing code

---
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/GLHandler.java
@@ -0,0 +1,94 @@
+package com.fARmework.modules.SpaceGraphics;
+
+import android.opengl.*;
+
+public class GLHandler 
+{
+	private final String VERTEX_SHADER_CODE =
+			"uniform mat4 u_MVPMatrix; " +
+			"attribute vec4 vPosition;" +
+			"void main() " +
+			"{" +
+			"	gl_Position = u_MVPMatrix * vPosition;" +
+			"}";
+	
+	private final String FRAGMENT_SHADER_CODE =
+			"precision mediump float;" +
+			"uniform vec4 vColor;" +
+			"void main()" +
+			"{" +
+			"	gl_FragColor = vColor;" +
+			"}";
+	
+	private int _vertexShader;
+	private int _fragmentShader;
+	
+	private final int _program;
+	private int _positionHandle;
+	private int _colorHandle;
+	private int _MVPMatrixHandle;
+	
+	public GLHandler()
+	{
+		_program = GLES20.glCreateProgram();
+		
+		initialize();
+	}
+	
+	public int getVertexShader()
+	{
+		return _vertexShader;
+	}
+	
+	public int getFragmentShader()
+	{
+		return _fragmentShader;
+	}
+	
+	public int getProgram()
+	{
+		return _program;
+	}
+	
+	public int getPositionHandle()
+	{
+		return _positionHandle;
+	}
+	
+	public int getColorHandle()
+	{
+		return _colorHandle;
+	}
+	
+	public int getMVPMatrixHandle()
+	{
+		return _MVPMatrixHandle;
+	}
+	
+	private void initialize()
+	{		
+		_vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER_CODE);
+		_fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER_CODE);
+		
+		GLES20.glAttachShader(_program, _vertexShader);
+		GLES20.glAttachShader(_program, _fragmentShader);
+		GLES20.glLinkProgram(_program);
+		
+		GLES20.glUseProgram(_program);
+		
+		_positionHandle = GLES20.glGetAttribLocation(_program, "vPosition");
+		
+        _colorHandle = GLES20.glGetUniformLocation(_program, "vColor");
+        _MVPMatrixHandle = GLES20.glGetUniformLocation(_program, "u_MVPMatrix");
+	}
+	
+	private int loadShader(int type, String code)
+	{
+		int shader = GLES20.glCreateShader(type);
+		
+		GLES20.glShaderSource(shader, code);
+		GLES20.glCompileShader(shader);
+		
+		return shader;
+	}
+}
--- development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/GraphicsRenderer.java
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/GraphicsRenderer.java
@@ -36,44 +36,14 @@ public class GraphicsRenderer implements GLSurfaceView.Renderer
         // such as screen rotation
         GLES20.glViewport(0, 0, width, height);
     }
-
-    public static int loadShader(int type, String shaderCode){
-
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-        int shader = GLES20.glCreateShader(type);
-
-        // add the source code to the shader and compile it
-        GLES20.glShaderSource(shader, shaderCode);
-        GLES20.glCompileShader(shader);
-
-        return shader;
-    }
-
 }
 
-class Triangle {
-
-    private final String vertexShaderCode =
-    	"uniform mat4 u_MVPMatrix; " +
-        "attribute vec4 vPosition;" +
-        "void main() {" +
-        "  gl_Position = u_MVPMatrix * vPosition;" +
-        "}";
-
-    private final String fragmentShaderCode =
-        "precision mediump float;" +
-        "uniform vec4 vColor;" +
-        "void main() {" +
-        "  gl_FragColor = vColor;" +
-        "}";
+class Triangle 
+{
+	private final GLHandler _glHandler = new GLHandler();
 
     private final FloatBuffer vertexBuffer;
     private final ByteBuffer indexBuffer;
-    private final int mProgram;
-    private int mPositionHandle;
-    private int mColorHandle;
-    private int mMVPMatrixHandle;
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
@@ -136,42 +106,19 @@ class Triangle {
         indexBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
         indexBuffer.put(indices);
         indexBuffer.position(0);
-
-        // prepare shaders and OpenGL program
-        int vertexShader = GraphicsRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
-                                                   vertexShaderCode);
-        int fragmentShader = GraphicsRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
-                                                     fragmentShaderCode);
-
-        mProgram = GLES20.glCreateProgram();             // create empty OpenGL Program
-        GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
-        GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
-        GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
-
     }
 
-    public void draw() {
-        // Add program to OpenGL environment
-        GLES20.glUseProgram(mProgram);
-
-        // get handle to vertex shader's vPosition member
-        mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-
-        // Enable a handle to the triangle vertices
-        GLES20.glEnableVertexAttribArray(mPositionHandle);
-
+    public void draw() 
+    {
         // Prepare the triangle coordinate data
-        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+        GLES20.glVertexAttribPointer(_glHandler.getPositionHandle(), COORDS_PER_VERTEX,
                                      GLES20.GL_FLOAT, false,
                                      vertexStride, vertexBuffer);
 
-        // get handle to fragment shader's vColor member
-        mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+		GLES20.glEnableVertexAttribArray(_glHandler.getPositionHandle());
         
-        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "u_MVPMatrix");
-
         // Set color for drawing the triangle
-        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+        GLES20.glUniform4fv(_glHandler.getColorHandle(), 1, color, 0);
 
         float rotationMatrix[] = new float[16];
         
@@ -179,13 +126,13 @@ class Triangle {
         Matrix.rotateM(rotationMatrix, 0, -20.0f, 0.0f, 1.0f, 0.0f);
         Matrix.rotateM(rotationMatrix, 0, -20.0f, 1.0f, 0.0f, 0.0f);
         
-        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, rotationMatrix, 0);
+        GLES20.glUniformMatrix4fv(_glHandler.getMVPMatrixHandle(), 1, false, rotationMatrix, 0);
         
         // Draw the triangle
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, 60, GLES20.GL_UNSIGNED_BYTE, indexBuffer);
         //GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
 
         // Disable vertex array
-        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(_glHandler.getPositionHandle());
     }
 }
