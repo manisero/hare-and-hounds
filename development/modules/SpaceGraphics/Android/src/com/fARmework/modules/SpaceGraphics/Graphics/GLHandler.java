5a19140e91e8b894360ddd2c4fda616507dcba9a
Kuba <Kuba@kubPC.Home>
2012-09-21 21:03:26 +0200

    Major refactoring, made GLHandler the only class to use OpenGL API

--- development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/GLHandler.java
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/GLHandler.java
@@ -1,9 +1,13 @@
 package com.fARmework.modules.SpaceGraphics.Graphics;
 
+import com.fARmework.modules.SpaceGraphics.Graphics.Models.*;
+
 import android.opengl.*;
 
 public class GLHandler 
 {
+	private final int COORDINATES_PER_VERTEX = 3;
+	
 	private final String VERTEX_SHADER_CODE =
 			"uniform mat4 u_MVPMatrix; " +
 			"attribute vec4 vPosition;" +
@@ -35,36 +39,6 @@ public class GLHandler
 		initialize();
 	}
 	
-	public int getVertexShader()
-	{
-		return _vertexShader;
-	}
-	
-	public int getFragmentShader()
-	{
-		return _fragmentShader;
-	}
-	
-	public int getProgram()
-	{
-		return _program;
-	}
-	
-	public int getPositionHandle()
-	{
-		return _positionHandle;
-	}
-	
-	public int getColorHandle()
-	{
-		return _colorHandle;
-	}
-	
-	public int getMVPMatrixHandle()
-	{
-		return _MVPMatrixHandle;
-	}
-	
 	private void initialize()
 	{		
 		_vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER_CODE);
@@ -91,4 +65,48 @@ public class GLHandler
 		
 		return shader;
 	}
+	
+	public void setViewport(int width, int height) 
+	{
+		GLES20.glViewport(0, 0, width, height);
+	}
+	
+	public void draw(Model model) 
+	{
+		float[] backgroundColor = model.getBackgroundColor();
+		
+		GLES20.glClearColor(backgroundColor[0], 
+							backgroundColor[1],
+							backgroundColor[2],
+							backgroundColor[3]);
+		
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		
+        GLES20.glVertexAttribPointer(_positionHandle, 
+        							 COORDINATES_PER_VERTEX,
+        							 GLES20.GL_FLOAT,
+        							 false,
+        							 COORDINATES_PER_VERTEX * 4, 
+        							 model.getVertexBuffer());
+
+		GLES20.glEnableVertexAttribArray(_positionHandle);
+        
+        GLES20.glUniform4fv(_colorHandle, 1, model.getColor(), 0);
+
+	    float rotationMatrix[] = new float[16];
+	        
+	    Matrix.setIdentityM(rotationMatrix, 0);
+	    
+	    float modelRotation[] = model.getRotation();
+	    
+	    Matrix.rotateM(rotationMatrix, 0, modelRotation[0], 1.0f, 0.0f, 0.0f);
+	    Matrix.rotateM(rotationMatrix, 0, modelRotation[1], 0.0f, 1.0f, 0.0f);
+	    Matrix.rotateM(rotationMatrix, 0, modelRotation[2], 0.0f, 0.0f, 1.0f);
+	        
+	    GLES20.glUniformMatrix4fv(_MVPMatrixHandle, 1, false, rotationMatrix, 0);
+	        
+	    GLES20.glDrawElements(GLES20.GL_TRIANGLES, 60, GLES20.GL_UNSIGNED_BYTE, model.getIndexBuffer());
+
+        GLES20.glDisableVertexAttribArray(_MVPMatrixHandle);		
+	}	
 }
