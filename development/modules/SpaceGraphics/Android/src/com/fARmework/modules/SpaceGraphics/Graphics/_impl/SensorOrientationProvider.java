de4a85addbd3e1d42bc9e09aaeccb2690cea17e6
Kuba <Kuba@kubPC.Home>
2012-09-21 20:41:34 +0200

    Made providers use TYPE_ORIENTATION sensor, fixed 2D arrow rotation

--- development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SensorOrientationProvider.java
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SensorOrientationProvider.java
@@ -2,21 +2,15 @@ package com.fARmework.modules.SpaceGraphics.Graphics._impl;
 
 import android.content.*;
 import android.hardware.*;
-import android.widget.*;
 
-import com.fARmework.modules.SpaceGraphics.Graphics.IOrientationProvider;
+import com.fARmework.modules.SpaceGraphics.Graphics.*;
 
 public class SensorOrientationProvider implements IOrientationProvider
 {
-	private Context mContext;
 	private SensorManager _sensorManager;
-	
-	private float[] _gravity = new float[3];
-	private float[] _geomagnetic = new float[3];
 	
 	public SensorOrientationProvider(Context context)
 	{
-		mContext = context;
 		_sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
 	}
 	
@@ -28,24 +22,6 @@ public class SensorOrientationProvider implements IOrientationProvider
 			@Override
 			public void onSensorChanged(final SensorEvent event)
 			{
-				_gravity = event.values;
-				//SensorOrientationProvider.this.onSensorChanged(orientationListener);
-			}
-			
-			@Override
-			public void onAccuracyChanged(Sensor sensor, int accuracy)
-			{
-			}
-		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-		
-		_sensorManager.registerListener(new SensorEventListener()
-		{
-			@Override
-			public void onSensorChanged(SensorEvent event)
-			{
-				_geomagnetic = event.values;
-				//SensorOrientationProvider.this.onSensorChanged(orientationListener);
-				
 				orientationListener.onOrientationChanged(event.values[0], event.values[1], event.values[2]);
 			}
 			
@@ -53,23 +29,6 @@ public class SensorOrientationProvider implements IOrientationProvider
 			public void onAccuracyChanged(Sensor sensor, int accuracy)
 			{
 			}
-		}, _sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
-	}
-	
-	private void onSensorChanged(IOrientationListener orientationListener)
-	{
-		float[] rotationMatrix = new float[9];
-		float[] values = new float[3];
-		
-		SensorManager.getRotationMatrix(rotationMatrix, null, _gravity, _geomagnetic);
-		SensorManager.getOrientation(rotationMatrix, values);
-		
-		orientationListener.onOrientationChanged(values[0] * 180.0f / 3.14f, values[1] * 180.0f / 3.14f, values[2] * 180.0f / 3.14f);
-		
-		/*
-		Toast.makeText(mContext, "A: " + values[0] * 180.0 / 3.14 + "\nP: " + values[1] * 180.0 / 3.14 + "\nR: " + values[2] * 180.0 / 3.14, Toast.LENGTH_SHORT).show();
-		try { Thread.sleep(2000); }
-		catch (InterruptedException e) {}
-		*/
+		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 }
--- development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SimulatorOrientationProvider.java
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SimulatorOrientationProvider.java
@@ -3,7 +3,6 @@ package com.fARmework.modules.SpaceGraphics.Graphics._impl;
 import org.openintents.sensorsimulator.hardware.*;
 
 import android.content.*;
-import android.hardware.SensorManager;
 import android.os.*;
 
 import com.fARmework.modules.SpaceGraphics.Graphics.*;
@@ -11,9 +10,6 @@ import com.fARmework.modules.SpaceGraphics.Graphics.*;
 public class SimulatorOrientationProvider implements IOrientationProvider
 {
 	private SensorManagerSimulator _sensorManager;
-	
-	private float[] _gravity = new float[3];
-	private float[] _geomagnetic = new float[3];
 	
 	public SimulatorOrientationProvider(Context context)
 	{
@@ -32,40 +28,13 @@ public class SimulatorOrientationProvider implements IOrientationProvider
 			@Override
 			public void onSensorChanged(final SensorEvent event)
 			{
-				_gravity = event.values;
-				SimulatorOrientationProvider.this.onSensorChanged(orientationListener);
+				orientationListener.onOrientationChanged(event.values[0], event.values[1], event.values[2]);
 			}
 			
 			@Override
 			public void onAccuracyChanged(Sensor sensor, int accuracy)
 			{
 			}
-		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManagerSimulator.SENSOR_DELAY_NORMAL);
-		
-		_sensorManager.registerListener(new SensorEventListener()
-		{
-			@Override
-			public void onSensorChanged(SensorEvent event)
-			{
-				_geomagnetic = event.values;
-				SimulatorOrientationProvider.this.onSensorChanged(orientationListener);
-			}
-			
-			@Override
-			public void onAccuracyChanged(Sensor sensor, int accuracy)
-			{
-			}
-		}, _sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManagerSimulator.SENSOR_DELAY_NORMAL);
-	}
-	
-	private void onSensorChanged(IOrientationListener orientationListener)
-	{
-		float[] rotationMatrix = new float[9];
-		float[] values = new float[3];
-		
-		SensorManager.getRotationMatrix(rotationMatrix, null, _gravity, _geomagnetic);
-		SensorManager.getOrientation(rotationMatrix, values);
-		
-		orientationListener.onOrientationChanged(values[0], values[1], values[2]);
+		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManagerSimulator.SENSOR_DELAY_NORMAL);
 	}
 }
