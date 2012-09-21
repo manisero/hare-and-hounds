fc7f86b10f91ea8f151ded32f90cfd832a67859f
manisero <manisero@manisero-PC>
2012-09-21 22:01:09 +0200

    Implemented SesnorManager.getOrientation usage

--- development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SensorOrientationProvider.java
+++ development/modules/SpaceGraphics/Android/src/com/fARmework/modules/SpaceGraphics/Graphics/_impl/SensorOrientationProvider.java
@@ -9,6 +9,9 @@ public class SensorOrientationProvider implements IOrientationProvider
 {
 	private SensorManager _sensorManager;
 	
+	private float[] _gravity = new float[3];
+	private float[] _geomagnetic = new float[3];
+	
 	public SensorOrientationProvider(Context context)
 	{
 		_sensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
@@ -22,13 +25,46 @@ public class SensorOrientationProvider implements IOrientationProvider
 			@Override
 			public void onSensorChanged(final SensorEvent event)
 			{
-				orientationListener.onOrientationChanged(event.values[0], event.values[1], event.values[2]);
+				System.arraycopy(event.values, 0, _gravity, 0, 3);
+				SensorOrientationProvider.this.onOrientationChanged(orientationListener);
 			}
 			
 			@Override
 			public void onAccuracyChanged(Sensor sensor, int accuracy)
 			{
 			}
-		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+		}, _sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+		
+		_sensorManager.registerListener(new SensorEventListener()
+		{
+			@Override
+			public void onSensorChanged(final SensorEvent event)
+			{
+				System.arraycopy(event.values, 0, _geomagnetic, 0, 3);
+				SensorOrientationProvider.this.onOrientationChanged(orientationListener);
+			}
+			
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy)
+			{
+			}
+		}, _sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+	}
+	
+	private void onOrientationChanged(IOrientationListener orientationListener)
+	{
+		float[] rotationMatrix = new float[9];
+		float[] values = new float[3];
+
+		if (SensorManager.getRotationMatrix(rotationMatrix, null, _gravity, _geomagnetic))
+		{
+			SensorManager.getOrientation(rotationMatrix, values);
+			
+			float azimuth = (float)Math.toDegrees(values[0]);
+			float pitch = (float)Math.toDegrees(values[1]);
+			float roll = (float)Math.toDegrees(values[2]);
+			
+			orientationListener.onOrientationChanged(azimuth, pitch, roll);
+		}
 	}
 }
