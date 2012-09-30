e3d41f1088c69e806a364c887ca588a8e2990ae4
Kuba <Kuba@kubPC.Home>
2012-09-30 19:58:26 +0200

    Fixed calculateDirection() method

--- development/modules/PositionTracking/Java/src/com/fARmework/modules/PositionTracking/Java/_impl/DirectionCalculator.java
+++ development/modules/PositionTracking/Java/src/com/fARmework/modules/PositionTracking/Java/_impl/DirectionCalculator.java
@@ -7,14 +7,18 @@ public class DirectionCalculator implements IDirectionCalculator
 {
 	public double calculateDirection(PositionData current, PositionData target)
 	{
-		double x = target.Latitude - current.Latitude;
-		double y = target.Longitude - current.Longitude;
+		double x = target.Longitude - current.Longitude;
+		double y = target.Latitude - current.Latitude;
 		
-		double direction = Math.atan2(y, x);
+		double direction = 0.5 * Math.PI - Math.atan2(y, x);
 		
 		if(direction < 0)
 		{
 			direction += 2 * Math.PI;
+		}
+		else if(direction > 2 * Math.PI)
+		{
+			direction -= 2 * Math.PI;
 		}
 		
 		return direction * 180 / Math.PI;
