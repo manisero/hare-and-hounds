cca4b7838db38a3c47497a559e5951aeff4898e8
Kuba <Kuba@kubPC.Home>
2012-10-11 21:42:03 +0200

    Implemented delay using timers rather than direct invocation of Thread

--- creativity/Android/SoundIndicator/gen/com/fARmework/Creativity/SoundIndicator/R.java
+++ creativity/Android/SoundIndicator/gen/com/fARmework/Creativity/SoundIndicator/R.java
@@ -15,8 +15,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080001;
+        public static final int menu_settings=0x7f080002;
         public static final int playButton=0x7f080000;
+        public static final int stopButton=0x7f080001;
     }
     public static final class layout {
         public static final int activity_sound=0x7f030000;
@@ -32,6 +33,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int play_button_caption=0x7f050004;
+        public static final int stop_button_caption=0x7f050005;
         public static final int title_activity_sound=0x7f050003;
     }
     public static final class style {
