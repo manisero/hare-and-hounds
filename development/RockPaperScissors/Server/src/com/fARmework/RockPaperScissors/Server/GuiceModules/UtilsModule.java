98ec1faa30b2a17df4a17a6ddfad0f6d9d60f118
Kuba <Kuba@kubPC.Home>
2012-10-21 18:59:10 +0200

    Implemented reading settings from XML file in RPS server

---
+++ development/RockPaperScissors/Server/src/com/fARmework/RockPaperScissors/Server/GuiceModules/UtilsModule.java
@@ -0,0 +1,14 @@
+package com.fARmework.RockPaperScissors.Server.GuiceModules;
+
+import com.fARmework.utils.Java.*;
+import com.fARmework.utils.Java._impl.*;
+import com.google.inject.*;
+
+public class UtilsModule extends AbstractModule
+{
+	@Override
+	protected void configure()
+	{
+		bind(ISettingsReader.class).to(SettingsReader.class);
+	}
+}
