65ca539f9abba54c49739d1e2adfbeefded72e9f
Kuba <Kuba@kublap.Home>
2012-10-22 21:02:22 +0200

    Quick and dirty integration of creativity project with development module

--- development/modules/SpaceGestures/Java/src/com/fARmework/modules/SpaceGestures/Java/_impl/SpaceGestureRecognizer.java
+++ development/modules/SpaceGestures/Java/src/com/fARmework/modules/SpaceGestures/Java/_impl/SpaceGestureRecognizer.java
@@ -3,6 +3,7 @@ package com.fARmework.modules.SpaceGestures.Java._impl;
 import com.fARmework.modules.SpaceGestures.Data.*;
 import com.fARmework.modules.SpaceGestures.Java.*;
 import com.fARmework.modules.SpaceGestures.Java.Matching.*;
+import com.fARmework.modules.SpaceGestures.Java.Processing.*;
 import com.google.inject.*;
 import java.util.*;
 
@@ -10,12 +11,14 @@ public class SpaceGestureRecognizer implements ISpaceGestureRecognizer
 {
 	private final ISpaceGestureRegistry _gestureRegistry;
 	private final ISpacePatternMatcherFactory _matcherFactory;
+	private final ISpaceGestureProcessor _processor;
 	
 	@Inject
-	public SpaceGestureRecognizer(ISpaceGestureRegistry gestureRegistry, ISpacePatternMatcherFactory matcherFactory)
+	public SpaceGestureRecognizer(ISpaceGestureRegistry gestureRegistry, ISpacePatternMatcherFactory matcherFactory, ISpaceGestureProcessor processor)
 	{
 		_gestureRegistry = gestureRegistry;
 		_matcherFactory = matcherFactory;
+		_processor = processor;
 	}
 	
 	@Override
@@ -23,11 +26,13 @@ public class SpaceGestureRecognizer implements ISpaceGestureRecognizer
 	{
 		List<SpaceGesture> gestures = _gestureRegistry.getGestures();
 		
+		List<Direction> moves = _processor.process(data);
+		
 		for(SpaceGesture gesture : gestures)
 		{
 			ISpacePatternMatcher matcher = _matcherFactory.get(gesture.getClass());
 			
-			if(matcher.match(data.Directions.toArray(new SpaceGestureData.Direction[0]), gesture.getPattern()))
+			if(matcher.match(moves.toArray(new Direction[0]), gesture.getPattern()))
 			{
 				return gesture.getName();
 			}
