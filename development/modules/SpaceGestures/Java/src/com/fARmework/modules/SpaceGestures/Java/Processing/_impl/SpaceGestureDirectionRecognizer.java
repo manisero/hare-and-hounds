94965314bac2b27f9be28a8fb84dcc33365917e2
Kuba <Kuba@kublap.Home>
2012-10-27 14:36:27 +0200

    Integrated changes with SpaceGestures.Java module

--- development/modules/SpaceGestures/Java/src/com/fARmework/modules/SpaceGestures/Java/Processing/_impl/SpaceGestureDirectionRecognizer.java
+++ development/modules/SpaceGestures/Java/src/com/fARmework/modules/SpaceGestures/Java/Processing/_impl/SpaceGestureDirectionRecognizer.java
@@ -9,79 +9,288 @@ import com.fARmework.modules.SpaceGestures.Java.Processing.*;
 
 public class SpaceGestureDirectionRecognizer implements ISpaceGestureDirectionRecognizer
 {
+	private static float THRESHOLD = 0.7f;
+	
 	@Override
 	public List<Direction> getMoveDirections(SpaceGestureData gesture, List<GestureRange> segments)
 	{
-		List<Direction> moves = new ArrayList<Direction>();
+		List<Direction> moves = new LinkedList<Direction>();
 		
-		for (GestureRange segment : segments)
+		for (GestureRange range : segments)
 		{
-			AccelerometerData maxAccelerations = getMaxAccelerations(segment, gesture.Readings);
+			AccelerometerData maxAccelerations = getMaxAccelerations(range, gesture);
+			AccelerometerData meanAccelerations = getMeanAccelerations(range, gesture);
 			
-			float x = Math.abs(maxAccelerations.X);
-			float y = Math.abs(maxAccelerations.Y);
-			float z = Math.abs(maxAccelerations.Z);
+			float x = Math.abs(meanAccelerations.X);
+			float y = Math.abs(meanAccelerations.Y);
+			float z = Math.abs(meanAccelerations.Z);
+						
+			boolean hasX = false;
+			boolean hasY = false;
+			boolean hasZ = false;
 			
 			if (x >= y && x >= z)
 			{
-				if (maxAccelerations.X >= 0)
+				hasX = true;
+				
+				if (y >= x * THRESHOLD)
 				{
-					moves.add(Direction.Left);
+					hasY = true;
 				}
-				else
+				
+				if(z >= x * THRESHOLD)
 				{
-					moves.add(Direction.Right);
+					hasZ = true;
 				}
 			}
 			else if (y >= x && y >= z)
 			{
-				if (maxAccelerations.Y >= 0)
+				hasY = true;
+				
+				if (x >= y * THRESHOLD)
 				{
-					moves.add(Direction.Down);
+					hasX = true;
 				}
-				else
+				
+				if (z >= y * THRESHOLD)
 				{
-					moves.add(Direction.Up);
+					hasZ = true;
 				}
 			}
 			else if (z >= x && z >= y)
 			{
-				if (maxAccelerations.Z >= 0)
+				hasZ = true;
+				
+				if (x >= z * THRESHOLD)
 				{
-					moves.add(Direction.Forward);
+					hasX = true;
 				}
-				else
+				
+				if (y >= z * THRESHOLD)
 				{
-					moves.add(Direction.Backward);
+					hasY = true;
 				}
 			}
+			
+			moves.add(getSingleDirection(maxAccelerations, hasX, hasY, hasZ));
 		}
 		
 		return moves;
 	}
-
-	private SpaceGestureData.AccelerometerData getMaxAccelerations(GestureRange range, List<SpaceGestureData.AccelerometerData> accelerationValues)
+	
+	private AccelerometerData getMeanAccelerations(GestureRange range, SpaceGestureData gestureData)
 	{
-		SpaceGestureData.AccelerometerData maxAccelerations = new SpaceGestureData.AccelerometerData(0.0f, 0.0f, 0.0f);
+		AccelerometerData[] accelerations = gestureData.Readings.toArray(new AccelerometerData[] {});
+		
+		AccelerometerData meanAccelerations = new AccelerometerData(0.0f, 0.0f, 0.0f);
+		
+		int amount = (range.LastSample + 1) - range.FirstSample;
 		
 		for (int i = range.FirstSample; i <= range.LastSample; ++i)
 		{
-			if (Math.abs(accelerationValues.get(i).X) > Math.abs(maxAccelerations.X))
+			meanAccelerations.X += Math.abs(accelerations[i].X);
+			meanAccelerations.Y += Math.abs(accelerations[i].Y);
+			meanAccelerations.Z += Math.abs(accelerations[i].Z);
+		}
+		
+		meanAccelerations.X /= amount;
+		meanAccelerations.Y /= amount;
+		meanAccelerations.Z /= amount;
+		
+		return meanAccelerations;
+	}
+	
+	private AccelerometerData getMaxAccelerations(GestureRange range, SpaceGestureData gestureData)
+	{
+		AccelerometerData[] accelerations = gestureData.Readings.toArray(new AccelerometerData[] {});
+		
+		AccelerometerData maxAccelerations = new AccelerometerData(0.0f, 0.0f, 0.0f);
+		
+		for (int i = range.FirstSample; i <= range.LastSample; ++i)
+		{
+			if (Math.abs(accelerations[i].X) > Math.abs(maxAccelerations.X))
 			{
-				maxAccelerations.X = accelerationValues.get(i).X;
+				maxAccelerations.X = accelerations[i].X;
 			}
 			
-			if (Math.abs(accelerationValues.get(i).Y) > Math.abs(maxAccelerations.Y))
+			if (Math.abs(accelerations[i].Y) > Math.abs(maxAccelerations.Y))
 			{
-				maxAccelerations.Y = accelerationValues.get(i).Y;
+				maxAccelerations.Y = accelerations[i].Y;
 			}
 			
-			if (Math.abs(accelerationValues.get(i).Z) > Math.abs(maxAccelerations.Z))
+			if (Math.abs(accelerations[i].Z) > Math.abs(maxAccelerations.Z))
 			{
-				maxAccelerations.Z = accelerationValues.get(i).Z;
+				maxAccelerations.Z = accelerations[i].Z;
 			}
 		}
 		
 		return maxAccelerations;
+	}
+	
+	private Direction getSingleDirection(AccelerometerData maxAccelerations, boolean hasX, boolean hasY, boolean hasZ)
+	{
+		if (hasX && hasY && hasZ)
+		{
+			if (maxAccelerations.X >= 0)
+			{
+				if (maxAccelerations.Y >= 0)
+				{
+					if (maxAccelerations.Z >= 0)
+					{
+						return Direction.LeftDownForward;
+					}
+					else
+					{
+						return Direction.LeftDownBackward;
+					}
+				}
+				else
+				{
+					if (maxAccelerations.Z >= 0)
+					{
+						return Direction.LeftUpForward;
+					}
+					else
+					{
+						return Direction.LeftUpBackward;
+					}					
+				}
+			}
+			else
+			{
+				if (maxAccelerations.Y >= 0)
+				{
+					if (maxAccelerations.Z >= 0)
+					{
+						return Direction.RightDownForward;
+					}
+					else
+					{
+						return Direction.RightDownBackward;
+					}
+				}
+				else
+				{
+					if (maxAccelerations.Z >= 0)
+					{
+						return Direction.RightUpForward;
+					}
+					else
+					{
+						return Direction.RightUpBackward;
+					}					
+				}				
+			}
+		}
+		else if (hasX && hasY)
+		{
+			if (maxAccelerations.X >= 0)
+			{
+				if (maxAccelerations.Y >= 0)
+				{
+					return Direction.LeftDown;
+				}
+				else
+				{
+					return Direction.LeftUp;
+				}
+			}
+			else
+			{
+				if (maxAccelerations.Y >= 0)
+				{
+					return Direction.RightDown;
+				}
+				else
+				{
+					return Direction.RightUp;
+				}				
+			}
+		}
+		else if (hasX && hasZ)
+		{
+			if (maxAccelerations.X >= 0)
+			{
+				if (maxAccelerations.Z >= 0)
+				{
+					return Direction.LeftForward;
+				}
+				else
+				{
+					return Direction.LeftBackward;
+				}
+			}
+			else
+			{
+				if (maxAccelerations.Z >= 0)
+				{
+					return Direction.RightForward;
+				}
+				else
+				{
+					return Direction.RightBackward;
+				}				
+			}			
+		}
+		else if (hasY && hasZ)
+		{
+			if (maxAccelerations.Y >= 0)
+			{
+				if (maxAccelerations.Z >= 0)
+				{
+					return Direction.DownForward;
+				}
+				else
+				{
+					return Direction.DownBackward;
+				}
+			}
+			else
+			{
+				if (maxAccelerations.Z >= 0)
+				{
+					return Direction.UpForward;
+				}
+				else
+				{
+					return Direction.UpBackward;
+				}				
+			}			
+		}
+		else if (hasX)
+		{
+			if (maxAccelerations.X >= 0)
+			{
+				return Direction.Left;
+			}
+			else
+			{
+				return Direction.Right;
+			}
+		}
+		else if (hasY)
+		{
+			if (maxAccelerations.Y >= 0)
+			{
+				return Direction.Down;
+			}
+			else
+			{
+				return Direction.Up;
+			}			
+		}
+		else if (hasZ)
+		{
+			if (maxAccelerations.Z >= 0)
+			{
+				return Direction.Forward;
+			}
+			else
+			{
+				return Direction.Backward;
+			}			
+		}
+		
+		return Direction.Unknown;
 	}
 }
