9d3a19af4d2eafbb3cb48c8e142d4ac1e3175d2b
manisero <manisero@manisero-PC>
2012-11-05 00:33:03 +0100

    Added PlayCheckpointSound setting to SettingsProvider

--- development/HareAndHounds/Client/gen/com/fARmework/core/client/R.java
+++ development/HareAndHounds/Client/gen/com/fARmework/core/client/R.java
@@ -13,6 +13,9 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class bool {
+        public static final int defaultPlayCheckpointSound=0x7f090000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
