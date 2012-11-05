56b311150d0c311a7c5ad74cb5f06f18a157c89d
manisero <manisero@manisero-PC>
2012-11-05 21:37:01 +0100

    SpaceGraphicsView: implemented Android-Binding support

--- development/HareAndHounds/Client/src/com/fARmework/HareAndHounds/Client/Activities/CheckpointActivity.java
+++ development/HareAndHounds/Client/src/com/fARmework/HareAndHounds/Client/Activities/CheckpointActivity.java
@@ -1,23 +1,8 @@
 package com.fARmework.HareAndHounds.Client.Activities;
 
-import android.os.*;
-import android.view.*;
-
 import com.fARmework.HareAndHounds.Client.ViewModels.*;
-import com.fARmework.modules.SpaceGraphics.Android.*;
-import com.fARmework.modules.SpaceGraphics.Android.Projection.*;
 import com.fARmework.utils.Android.Activities.*;
 
 public class CheckpointActivity extends BoundActivity<CheckpointViewModel>
 {
-	@Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        
-        SpaceGraphicsView spaceGraphicsView = (SpaceGraphicsView)((ViewGroup)View).getChildAt(0); // TODO: Find the view by id once SpaceGraphicsView's constructor is fixed
-        IGraphicsRenderer graphicsRenderer = spaceGraphicsView.getGraphicsRenderer();
-        graphicsRenderer.setDirectionProvider(ViewModel.getDirectionProvider());
-        graphicsRenderer.setModel(ViewModel.getArrowModel());
-    }
 }
